922f9acb871d8e911e11830b805dfecc3b899ff9
jordan819 <patryk.zaucha00@gmail.com>
2024-07-10 22:50:52 +0200

    use local storage to prevent data loss on refresh

--- src/app/service/doctor/doctor.service.ts
+++ src/app/service/doctor/doctor.service.ts
@@ -5,12 +5,28 @@ import {DoctorModel} from '../../model/doctor.model';
   providedIn: 'root'
 })
 export class DoctorService {
-  private doctors: DoctorModel[] = [
-    {pwz: '124322', name: 'John K'},
-    {pwz: '2221111', name: 'Jane D'},
-  ];
+  private readonly storageKey = 'doctors';
 
   constructor() {
+    this.loadDoctors();
+  }
+
+  private doctors: DoctorModel[] = [];
+
+  private saveDoctors() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.doctors));
+  }
+
+  private loadDoctors() {
+    const savedDoctors = localStorage.getItem(this.storageKey);
+    if (savedDoctors) {
+      this.doctors = JSON.parse(savedDoctors);
+    } else {
+      this.doctors = [
+        {pwz: '124322', name: 'John K'},
+        {pwz: '2221111', name: 'Jane D'}
+      ];
+    }
   }
 
   getDoctors(): DoctorModel[] {
@@ -19,13 +35,16 @@ export class DoctorService {
 
   addDoctor(doctor: DoctorModel) {
     this.doctors.push(doctor);
+    this.saveDoctors();
   }
 
   updateDoctor(index: number, doctor: DoctorModel) {
     this.doctors[index] = doctor;
+    this.saveDoctors();
   }
 
   deleteDoctor(index: number) {
     this.doctors.splice(index, 1);
+    this.saveDoctors();
   }
 }
